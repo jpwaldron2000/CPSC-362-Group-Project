4fa0919bd6cd4ce13b1bd436336c589cb70f5c8e
carsonyuwong <60160331+carsonyuwong@users.noreply.github.com>
2022-04-04 20:57:23 -0700

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Mover
+{
+    private SpriteRenderer spriteRenderer;
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void RecieveDamage(Damage dmg)
+    {
+        base.RecieveDamage(dmg);
+        GameManager.instance.OnHitpointChange();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        UpdateMotor(new Vector3(x, y, 0));
+    }
+
+    public void SwapSprite(int skinID)
+    {
+        GetComponent<SpriteRenderer>().sprite = GameManager.instance.playerSprites[skinID];
+    }
+
+    public void OnLevelUp()
+    {
+        maxHitpoint++;
+        hitpoint = maxHitpoint;
+    }
+
+    public void SetLevel(int level)
+    {
+        for (int i = 0; i < level; i++)
+            OnLevelUp();
+    }
+
+    public void Heal(int healingAmount)
+    {
+        if (hitpoint == maxHitpoint)
+            return;
+
+        hitpoint += healingAmount;
+        if (hitpoint > maxHitpoint)
+            hitpoint = maxHitpoint;
+        GameManager.instance.ShowText("+" + healingAmount.ToString() + "hp", 25, Color.green, transform.position, Vector3.up * 30, 1.0f);
+        GameManager.instance.OnHitpointChange();
+    }
+}
