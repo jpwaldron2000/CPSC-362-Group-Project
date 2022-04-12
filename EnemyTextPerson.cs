dbbcb5f9641f05eb704b277601f40dab261e699d
carsonyuwong <60160331+carsonyuwong@users.noreply.github.com>
2022-04-11 22:12:31 -0700

    Custom script
    Slightly modified the NPCText script to allow enemies to say random lines and taunts. Just attach to an enemy gameobject, specify how many lines there are and how large you want the text and fill in the lines into the textboxes.

---
+++ EnemyTextPerson.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTextPerson : Collidable
+{
+    public string[] message;
+    public int textSize;
+    private float coolDown = 4.0f;
+    private float lastShout;
+
+    protected override void Start()
+    {
+        base.Start();
+        lastShout = -coolDown;
+    }
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (Time.time - lastShout > coolDown)
+        {
+            lastShout = Time.time;
+            GameManager.instance.ShowText(message[Random.Range(0, message.Length)], textSize, Color.red, transform.position + new Vector3(0, 0.16f, 0), Vector3.zero, coolDown);
+        }
+    }
+}
